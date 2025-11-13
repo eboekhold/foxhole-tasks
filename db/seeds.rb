5e78bc456aba8c3edb91a185f3ac147d4742d9b4
Eva <eva.boekhold@gmail.com>
2025-11-13 22:07:43 +0100

    Add more refinery recipes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,17 @@
 #   end
 
 salvage = FactoryBot.create(:resource, name: "Salvage")
+component = FactoryBot.create(:resource, name: "Component")
+sulfur = FactoryBot.create(:resource, name: "Sulfur")
+coal = FactoryBot.create(:resource, name: "Coal")
 bmat = FactoryBot.create(:resource, name: "Basic Material")
+emat = FactoryBot.create(:resource, name: "Explosive Powder")
+hemat = FactoryBot.create(:resource, name: "Heavy Explosive Powder")
+rmat = FactoryBot.create(:resource, name: "Refined Material")
+diesel = FactoryBot.create(:resource, name: "Diesel")
 refinery = FactoryBot.create(:structure, name: "Refinery")
+FactoryBot.create(:recipe, structure: refinery, input_resources_attributes: [{resource: component, amount: 20}], output_resources_attributes: [{resource: rmat, amount: 1}])
+FactoryBot.create(:recipe, structure: refinery, input_resources_attributes: [{resource: salvage, amount: 10}], output_resources_attributes: [{resource: diesel, amount: 1}])
 FactoryBot.create(:recipe, structure: refinery, input_resources_attributes: [{resource: salvage, amount: 2}], output_resources_attributes: [{resource: bmat, amount: 1}])
+FactoryBot.create(:recipe, structure: refinery, input_resources_attributes: [{resource: salvage, amount: 5}], output_resources_attributes: [{resource: emat, amount: 1}])
+FactoryBot.create(:recipe, structure: refinery, input_resources_attributes: [{resource: sulfur, amount: 5}], output_resources_attributes: [{resource: hemat, amount: 1}])
